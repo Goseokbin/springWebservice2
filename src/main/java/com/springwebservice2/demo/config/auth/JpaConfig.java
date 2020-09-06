e949037aa8267dc8fcb37ee010913d9979ec6a73
seokbin <bini1050@naver.com>
2020-09-06 20:18:52 +0900

    EC2 배포 전 최종 수정

--- src/main/java/com/springwebservice2/demo/config/auth/JpaConfig.java
+++ src/main/java/com/springwebservice2/demo/config/auth/JpaConfig.java
@@ -1,9 +1,10 @@
 package com.springwebservice2.demo.config.auth;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
-@EnableWebSecurity
+@EnableJpaAuditing
 public class JpaConfig {
 }
--- src/main/java/com/springwebservice2/demo/web/IndexController.java
+++ src/main/java/com/springwebservice2/demo/web/IndexController.java
@@ -1,4 +1,4 @@
-package com.springwebservice2.demo;
+package com.springwebservice2.demo.web;
 
 import com.springwebservice2.demo.Service.posts.PostsService;
 import com.springwebservice2.demo.config.LoginUser;
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -23,7 +24,8 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user){
         List<PostListResponseDto> posts = postsService.findAllDesc();
-        model.addAttribute("posts", posts);
+        model.addAttribute("posts",
+                posts);
 
         if(user !=null){
             model.addAttribute("userName", user.getName());
